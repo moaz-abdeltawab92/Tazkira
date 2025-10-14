ba3790ed8f9d9f3d020612a8fe8ba1ec80674bad
moaz-abdeltawab92 <moazayman128@gmail.com>
2025-10-14 22:48:53 +0300

    add location tz to main production flavor

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tazkira_app/features/home/presentation/screens/homepage.dart';
-
+import 'package:timezone/data/latest.dart' as tz;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+tz.initializeTimeZones();
   // To fix text being hidden bug in flutter_screenutil in release mode
   await ScreenUtil.ensureScreenSize();
   runApp(const TazkiraApp());
