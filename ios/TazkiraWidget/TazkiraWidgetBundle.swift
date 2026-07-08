1a3caf98847e8770bd863cfce5b16e9b14361e1d
Hawazen Sameer <53709203+alheekmahlib@users.noreply.github.com>
2026-07-08 00:06:19 -0700

    ADD: WidgetKit for iOS

---
+++ ios/TazkiraWidget/TazkiraWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  TazkiraWidgetBundle.swift
+//  TazkiraWidget
+//
+//  Created by Hawazen Mahmood on 7/7/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TazkiraWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TazkiraWidget()
+        TazkiraWidgetControl()
+    }
+}
